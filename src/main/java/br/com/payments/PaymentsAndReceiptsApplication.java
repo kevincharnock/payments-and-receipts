3e824dd451afec4d9394eeb3c6405316ad81a528
Kevin Charnock <kevinshuldiner@gmail.com>
2023-11-27 18:08:21 -0300

    first commit

---
+++ src/main/java/br/com/payments/PaymentsAndReceiptsApplication.java
@@ -0,0 +1,13 @@
+package br.com.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsAndReceiptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsAndReceiptsApplication.class, args);
+	}
+
+}
